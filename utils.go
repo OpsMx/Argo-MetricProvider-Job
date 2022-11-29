d53b980214bbe3c78f0f956b2e263e619efb0d18
genos1998 <shaikhjunaid1998@gmail.com>
2022-11-29 10:54:50 +0530

    not needed
    Signed-off-by: genos1998 <shaikhjunaid1998@gmail.com>

--- utils.go
+++ utils.go
@@ -248,10 +248,10 @@ func getTemplateData(client http.Client, secretData map[string]string, template 
 		if err != nil {
 			return "", err
 		}
-		log.Infof("DEBUG - The value of templateCheckSave var is %v",templateCheckSave)
+		log.Infof("DEBUG - The value of templateCheckSave var is %v", templateCheckSave)
 		if templateCheckSave["errorMessage"] != "" && templateCheckSave["errorMessage"] != nil {
 			errorss := fmt.Sprintf("%v", templateCheckSave["errorMessage"])
-			log.Infof("DEBUG- %s",errorss)
+			log.Infof("DEBUG- %s", errorss)
 			err = errors.New(errorss)
 			return "", err
 		}
